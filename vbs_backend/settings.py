f1e6f93ab49ffcf44e1e697ba0114a9fadc816f4
guptamanas149@gmail.com <guptamanas149@gmail.com>
2024-04-23 01:21:04 +0530

    updated postgres

--- vbs_backend/settings.py
+++ vbs_backend/settings.py
@@ -101,13 +101,23 @@ WSGI_APPLICATION = 'vbs_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'manas',
+        'PASSWORD': 'kIsha2719$',
+        'HOST': 'database-2.cpey0ys8435d.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432',  # default PostgreSQL port
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
